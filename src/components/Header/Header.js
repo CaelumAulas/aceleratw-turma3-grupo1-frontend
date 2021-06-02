e115fb769b37ead648064a3d8975effa1642846c
André Luiz Boni Bezerra <andre.luiz@thoughtworks.com>
2021-06-02 12:06:44 -0300

    Adds icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,9 @@ import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
 import React from 'react'
 
 export default function Header() {
@@ -22,6 +25,14 @@ export default function Header() {
     <>
       <AppBar position="fixed" color="primary">
         <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
             CARANGO BOM
           </Typography>
