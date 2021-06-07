abfe67c89800f747bc12b45f79510f60f8fa425d
André Luiz Boni Bezerra <andre.luiz@thoughtworks.com>
2021-06-07 13:46:53 -0300

    Removes unused code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,9 @@
 import { Box } from '@material-ui/core'
 import AppBar from '@material-ui/core/AppBar'
-import IconButton from '@material-ui/core/IconButton'
 import Link from '@material-ui/core/Link'
 import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import MenuIcon from '@material-ui/icons/Menu'
 import React from 'react'
 import { Link as NavLink } from 'react-router-dom'
 
@@ -26,14 +24,6 @@ export default function Header() {
     <>
       <AppBar position="fixed" color="primary">
         <Toolbar>
-          {/* <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography variant="h6" className={classes.title}>
             <Link component={NavLink} to="/" color="inherit">
               Carango Bom
