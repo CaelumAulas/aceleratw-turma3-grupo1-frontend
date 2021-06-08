9530b4ad38c04e5f63799c4b73e4dc8aee2444a5
Guilherme Vezula <vezula18@gmail.com>
2021-06-08 15:03:57 -0300

    reverts to the original

--- src/components/UserRestrictedRoute/UserRestrictedRoute.jsx
+++ src/components/UserRestrictedRoute/UserRestrictedRoute.jsx
@@ -1,20 +1,13 @@
 import React, { useContext } from 'react'
 import { Redirect } from 'react-router'
-import { useParams, useRouteMatch } from 'react-router-dom'
 import UserLogged from '../../contexts/UserLogged'
-import useLocalStorage from '../../hooks/useLocalStorage'
 
 export default function UserRestrictedRoute({ children }) {
   const userLoggedContext = useContext(UserLogged)
-  const { path } = useRouteMatch()
-  const [pathUnauthorized, setpathUnauthorized, deletePathUnauthorized] =
-    useLocalStorage('pathUnauthorized', path)
 
   if (!userLoggedContext.user) {
-    setpathUnauthorized(pathUnauthorized)
     return <Redirect to="/login" />
   }
 
-  deletePathUnauthorized()
   return <>{children}</>
 }
