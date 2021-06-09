ffa06bed38a2b412776ae900e4de65d7ada37839
André Luiz Boni Bezerra <andre.luiz@thoughtworks.com>
2021-06-09 11:02:14 -0300

    Removes unused code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,17 @@
-import { Box, Container, CircularProgress } from '@material-ui/core'
+import { Box, Container } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../components/PageTitle/PageTitle'
 import VehicleTable from '../components/VehicleTable/VehicleTable'
-import ApiListVehicles from '../infraestructure/api/ApiListVehicles'
 import ApiDeleteVehicle from '../infraestructure/api/ApiDeleteVehicle'
+import ApiListVehicles from '../infraestructure/api/ApiListVehicles'
 
 export default function HomePage() {
   const [vehicles, setVehicles] = useState([])
-  const [loading, setLoading] = useState(true)
 
   async function callApiListVehicles() {
     try {
       const response = await ApiListVehicles()
       setVehicles(response.data)
-      setLoading(false)
     } catch (e) {}
   }
 
@@ -32,18 +30,15 @@ export default function HomePage() {
     <Container maxWidth="lg">
       <PageTitle title="Veículos disponíveis para compra" />
       <Box mt={4}>
-        {loading && <CircularProgress show />}
-        {!loading && (
-          <VehicleTable
-            vehicles={vehicles}
-            onEditHandler={() => alert('Redicionar para a página de edição')}
-            onDeleteHandler={async id => {
-              if (window.confirm('Tem certeza que deseja apagar este item?')) {
-                await callApiDeleteVehicle(id)
-              }
-            }}
-          />
-        )}
+        <VehicleTable
+          vehicles={vehicles}
+          onEditHandler={() => alert('Redicionar para a página de edição')}
+          onDeleteHandler={async id => {
+            if (window.confirm('Tem certeza que deseja apagar este item?')) {
+              await callApiDeleteVehicle(id)
+            }
+          }}
+        />
       </Box>
     </Container>
   )
