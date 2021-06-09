bda5a0bae65f814ded99eeb2fcebdf62b4e097a1
André Luiz Boni Bezerra <andre.luiz@thoughtworks.com>
2021-06-09 18:22:46 -0300

    Adds error modal on creating new vehicle

--- src/pages/CreateVehiclePage.js
+++ src/pages/CreateVehiclePage.js
@@ -8,10 +8,12 @@ import { ApiNewVehicle } from '../infraestructure/api/ApiVehicles'
 import {
   validateCarBrand,
   validateCarModel,
-  validateCarYear
+  validateCarYear,
 } from '../infraestructure/validations/form/form'
+import Modal from '../components/Modal/Modal'
 
 export default function CreateVehiclePage() {
+  const [showModal, setShowModal] = useState(false)
   const [formData, setFormData] = useState({})
   const history = useHistory()
 
@@ -28,33 +30,43 @@ export default function CreateVehiclePage() {
         alert('Novo veículo cadastrado')
         history.push('/')
       } else {
-        // setShowModal(true)
+        setShowModal(true)
       }
     } catch (e) {
-      // setShowModal(true)
+      setShowModal(true)
     }
   }
 
   return (
-    <Container maxWidth="xs">
-      <Paper elevation={3}>
-        <Box p={3}>
-          <PageTitle title="Novo veículo" />
+    <>
+      <Modal
+        title={'Erro inesperado!'}
+        description={
+          'Não foi possível criar o novo veículo. Por favor, tente novamente mais tarde.'
+        }
+        open={showModal}
+        onClose={() => setShowModal(false)}
+      />
+      <Container maxWidth="xs">
+        <Paper elevation={3}>
+          <Box p={3}>
+            <PageTitle title="Novo veículo" />
 
-          <VehicleFlowFormValidations.Provider
-            value={{
-              brand: validateCarBrand,
-              year: validateCarYear,
-              model: validateCarModel,
-            }}
-          >
-            <CreateVehicleForm
-              onChange={formData => setFormData(formData)}
-              onSubmit={callApiNewVehicle}
-            />
-          </VehicleFlowFormValidations.Provider>
-        </Box>
-      </Paper>
-    </Container>
+            <VehicleFlowFormValidations.Provider
+              value={{
+                brand: validateCarBrand,
+                year: validateCarYear,
+                model: validateCarModel,
+              }}
+            >
+              <CreateVehicleForm
+                onChange={formData => setFormData(formData)}
+                onSubmit={callApiNewVehicle}
+              />
+            </VehicleFlowFormValidations.Provider>
+          </Box>
+        </Paper>
+      </Container>
+    </>
   )
 }
